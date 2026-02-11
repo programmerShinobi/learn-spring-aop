4e121f48f2b704fba360b93e5889aa9ef45f47ad
programmerShinobi <shinobiprogrammer@gmail.com>
2026-02-11 15:04:21 +0700

    [add] learn : pointcut

--- src/main/java/programmershinobi/aop/aspect/LogAspect.java
+++ src/main/java/programmershinobi/aop/aspect/LogAspect.java
@@ -1,9 +1,16 @@
 package programmershinobi.aop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LogAspect {
+
+    @Pointcut("")
+    public void helloServiceMethod() {
+
+    }
+
 }
