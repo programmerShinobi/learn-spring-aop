c0a9bd93dd207d38287d7b7da0722c5d2ebeb029
programmerShinobi <shinobiprogrammer@gmail.com>
2026-02-11 11:39:21 +0700

    [add] first commit

---
+++ src/test/java/programmershinobi/aop/BelajarSpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package programmershinobi.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
