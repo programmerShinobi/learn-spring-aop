9fa597061aad2e9923820ce5d167fafab57eb207
programmerShinobi <shinobiprogrammer@gmail.com>
2026-02-11 15:20:02 +0700

    [add] learn : advice

--- src/main/java/programmershinobi/aop/aspect/LogAspect.java
+++ src/main/java/programmershinobi/aop/aspect/LogAspect.java
@@ -1,16 +1,29 @@
 package programmershinobi.aop.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class LogAspect {
 
     @Pointcut("target(programmershinobi.aop.service.HelloService)")
     public void helloServiceMethod() {
 
+    }
+
+    @Before("helloServiceMethod()")
+    public void beforeHelloServiceMethod() {
+        log.info("Before HelloService Method");
+    }
+
+    @Before("helloServiceMethod()")
+    public void beforeHelloServiceMethodAgain() {
+        log.info("Before HelloService Method Again");
     }
 
 }
