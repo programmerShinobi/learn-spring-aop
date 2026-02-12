f6a785bb2efde02d0067b6ff0ac60330e1ab5ea2
programmerShinobi <shinobiprogrammer@gmail.com>
2026-02-12 09:24:36 +0700

    [add] learn : proceeding join point

--- src/main/java/programmershinobi/aop/aspect/LogAspect.java
+++ src/main/java/programmershinobi/aop/aspect/LogAspect.java
@@ -2,6 +2,8 @@ package programmershinobi.aop.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -31,5 +33,22 @@ public class LogAspect {
     public void beforeHelloServiceMethodAgain() {
         log.info("Before HelloService Method Again");
     }
+
+    @Around("helloServiceMethod()")
+    public Object aroundHelloServiceMethod(ProceedingJoinPoint joinPoint) {
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+
+        try {
+            log.info("Around Before {}.{}({})", className, methodName, Arrays.toString(joinPoint.getArgs()));
+            return joinPoint.proceed(joinPoint.getArgs());
+        } catch (Throwable throwable) {
+            log.info("Around Error {}.{}({})", className, methodName, Arrays.toString(joinPoint.getArgs()));
+            return throwable;
+        } finally {
+            log.info("Around Finnaly {}.{}({})", className, methodName, Arrays.toString(joinPoint.getArgs()));
+        }
+    }
+
 
 }
